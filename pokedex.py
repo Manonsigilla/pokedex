61d7de47436ada8ee9dbcfbfa5323e581eead37f
Manon Sigaud <sigaudmanon@gmail.com>
2023-08-20 19:45:57 +0200

    Modifications Tentative d'ajouter du son a l ouverture de la fenetre mais echec, a retenter modifications diverses au niveau des couleurs, tailles, reliefs, emplacements, etc

--- pokedex.py
+++ pokedex.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import messagebox
+from playsound import playsound
+import threading
 
 fenetre = tk.Tk()
 fenetre.title("Pokedex v0.1")
@@ -10,11 +12,24 @@ fenetre.config(bg="red")
 fenetre.rowconfigure([i for i in range(4)], minsize=50, weight=1)
 fenetre.columnconfigure([i for i in range(3)], minsize=50, weight=1)
 
+# music settings
+
+# def play_music():
+#     playsound('Son/pokemontheme.mp3', False)
+    
+# def play_music():
+#     while fenetre.mainloop() is True:
+#         playsound('Son/pokemontheme.mp3', False)
+#         threading.Thread(target=playsound, args=("Son/pokemontheme.mp3",)).start()
+#     else:
+#         playsound('Son/pokemontheme.mp3', False)
+#         threading.Thread(target=playsound, args=("Son/pokemontheme.mp3",)).daemon()
+
 # Pokemon name
 
-pokemon_name_frame = tk.Frame(fenetre, bg="red", borderwidth=5, relief="sunken")
+pokemon_name_frame = tk.Frame(fenetre, bg="red", borderwidth=5, relief="raised")
 pokemon_name_frame.grid(row=0, column=0)
-pokemon_name = tk.Label(pokemon_name_frame, text="Nom du Pokemon", bg="white", background="#330000", fg="#ffcccc", font="Garamond 20 bold")
+pokemon_name = tk.Label(pokemon_name_frame, text="Nom du Pokemon", bg="white", background="#330000", fg="#ffcccc", font="Garamond 25 bold")
 pokemon_name.pack()
 
 # pokemon image and types
@@ -22,49 +37,63 @@ pokemon_name.pack()
 pokemon_image_frame = tk.Frame(fenetre, bg="red", borderwidth=4, relief="sunken")
 pokemon_image_frame.grid(row=1, column=0)
 
-pokemon_types_frame = tk.Label(pokemon_image_frame, text="Type du pokemon", bg="red", borderwidth=5, relief="sunken")
+pokemon_types_frame = tk.Label(pokemon_image_frame, text="Type du pokemon", bg="white", background="#330000", fg="#ffcccc", font="Garamond 14 bold", relief="sunken")
 pokemon_types_frame.grid(row=0, column=1)
-pokemon_number = tk.Label(pokemon_image_frame, text="Nombre du pokémon", bg="white", fg="#ffcccc", font="Garamond 12 bold")
+pokemon_number = tk.Label(pokemon_image_frame, text="Nombre du pokémon", bg="#ffcccc", fg="#7a0000", font="Garamond 12 bold")
 pokemon_number.grid(row=1, column=1)
-pokemon_stats_title = tk.Label(pokemon_image_frame, text="Stats :", bg="white", fg="#ffcccc", font="Garamond 12 bold")
-pokemon_stats_title.grid(row=2, column=1)
+pokemon_stats_title = tk.Label(pokemon_image_frame, text="Stats :", bg="#ffcccc", fg="#7a0000", font="Garamond 12 bold")
+pokemon_stats_title.grid(row=2, column=0)
 
 # pokemon stats
 
-pokemon_stats_frame = tk.Frame(fenetre, bg="red", borderwidth=3)
-pokemon_stats_frame.grid(row=2, column=0)
+pokemon_stats_frame = tk.Frame(fenetre, bg="red", borderwidth=3, relief="sunken")
+pokemon_stats_frame.grid(row=2)
 
+pokemon_stats_hptitle = tk.Label(pokemon_stats_frame, text="HP :", bg="#ffcccc", fg="#7a0000")
+pokemon_stats_hptitle.grid(row=0, column=0)
 pokemon_stats_hp = tk.Label(pokemon_stats_frame, text="HP: 110", bg="white", background="#330000", fg="#ffcccc", font="Garamond 12")
-pokemon_stats_hp.grid(row=0, column=0)
+pokemon_stats_hp.grid(row=0, column=1)
 
+pokemon_stats_atacktitle = tk.Label(pokemon_stats_frame, text="Attack :", bg="#ffcccc", fg="#7a0000")
+pokemon_stats_atacktitle.grid(row=1, column=0)
 pokemon_stats_attack = tk.Label(pokemon_stats_frame, text="Attack: 52", bg="white", background="#330000", fg="#ffcccc", font="Garamond 12")
-pokemon_stats_attack.grid(row=1, column=0)
+pokemon_stats_attack.grid(row=1, column=1)
 
+pokemon_stats_defensetitle = tk.Label(pokemon_stats_frame, text="Defense :", bg="#ffcccc", fg="#7a0000")
+pokemon_stats_defensetitle.grid(row=2, column=0)
 pokemon_stats_defense = tk.Label(pokemon_stats_frame, text="Defense: 52", bg="white", background="#330000", fg="#ffcccc", font="Garamond 12")
-pokemon_stats_defense.grid(row=2, column=0)
+pokemon_stats_defense.grid(row=2, column=1)
 
+pokemon_stats_spatacktitle = tk.Label(pokemon_stats_frame, text="Sp. Attack :", bg="#ffcccc", fg="#7a0000")
+pokemon_stats_spatacktitle.grid(row=3, column=0)
 pokemon_stats_spattack = tk.Label(pokemon_stats_frame, text="Sp. Attack: 52", bg="white", background="#330000", fg="#ffcccc", font="Garamond 12")
-pokemon_stats_spattack.grid(row=3, column=0)
+pokemon_stats_spattack.grid(row=3, column=1)
 
+pokemon_stats_spdefensetitle = tk.Label(pokemon_stats_frame, text="Sp. Defense :", bg="#ffcccc", fg="#7a0000")
+pokemon_stats_spdefensetitle.grid(row=4, column=0)
 pokemon_stats_spdefense = tk.Label(pokemon_stats_frame, text="Sp. Defense: 85", bg="white", background="#330000", fg="#ffcccc", font="Garamond 12")
-pokemon_stats_spdefense.grid(row=4, column=0)
+pokemon_stats_spdefense.grid(row=4, column=1)
 
+pokemon_stats_speedtitle = tk.Label(pokemon_stats_frame, text="Speed :", bg="#ffcccc", fg="#7a0000")
+pokemon_stats_speedtitle.grid(row=5, column=0)
 pokemon_stats_speed = tk.Label(pokemon_stats_frame, text="Speed: 52", bg="white", background="#330000", fg="#ffcccc", font="Garamond 12")
-pokemon_stats_speed.grid(row=5, column=0)
+pokemon_stats_speed.grid(row=5, column=1)
 
+pokemon_stats_totaltitle = tk.Label(pokemon_stats_frame, text="Total :", bg="#ffcccc", fg="#7a0000")
+pokemon_stats_totaltitle.grid(row=6, column=0)
 pokemon_stats_total = tk.Label(pokemon_stats_frame, text="Total: 1150", bg="white", background="#330000", fg="#ffcccc", font="Garamond 12 bold")
-pokemon_stats_total.grid(row=6, column=0)
+pokemon_stats_total.grid(row=6, column=1)
 
 # Configuration des boutons
 
-bouton_frame = tk.Frame(fenetre, bg="white", borderwidth=5, relief="raised")
+bouton_frame = tk.Frame(fenetre, bg="red", borderwidth=5, relief="raised")
 bouton_frame.grid(row=3, column=0)
 
 def create_buttons(i):
-    left_bouton = tk.Button(bouton_frame, text="Back", command=lambda:afficher_pokemon(i - 1), font = ("Garamond 16"))
+    left_bouton = tk.Button(bouton_frame, text="<- Back", command=lambda:afficher_pokemon(i - 1), font = ("Garamond 16"), relief="raised", bg="#43B14B")
     left_bouton.grid(row=0, column=0)
 
-    right_bouton = tk.Button(bouton_frame, text="Next", command=lambda:afficher_pokemon(i + 1), font = ("Garamond 16"))
+    right_bouton = tk.Button(bouton_frame, text="Next ->", command=lambda:afficher_pokemon(i + 1), font = ("Garamond 16"), relief="raised", bg="#43B14B")
     right_bouton.grid(row=0, column=1)
 
 # Search frame
@@ -72,7 +101,7 @@ def create_buttons(i):
 search_frame = tk.Frame(fenetre, bg="red", borderwidth=5, relief="raised")
 search_frame.grid(row=0, column=1, rowspan=3)
 
-search_label = tk.Label(search_frame, text="Search a Pokemon", bg="white", font = ("Garamond 14 bold"))
+search_label = tk.Label(search_frame, text="Search a Pokemon", bg="white", background="#330000", fg="#ffcccc", font="Garamond 20 bold")
 search_label.grid(row=0, column=0, columnspan=2)
 
 #Efface l'insert en cliquant dans le champ de saisie
@@ -94,62 +123,62 @@ search_label2.bind("<FocusIn>", temp_text)
 
 # Add pokemon frame
 
-add_frame = tk.Frame(fenetre, bg="#e60000", borderwidth=4, relief="sunken")
+add_frame = tk.Frame(fenetre, bg="#e60000", borderwidth=4, relief="raised")
 add_frame.config(height=1000)
 add_frame.grid(row=1, column=2, rowspan=3)
 
-add_label = tk.Label(add_frame, text="Add a Pokemon", bg="white", font = ("Garamond 14 bold"))
+add_label = tk.Label(add_frame, text="Add a Pokemon", bg="white", background="#330000", fg="#ffcccc", font="Garamond 20 bold")
 add_label.grid(row=0, column=0, columnspan=2)
 
 add_label_name = tk.Entry(add_frame, text="Enter the name", bg="white", fg="black")
-add_label_name.grid(row=1, column=0)
-add_label_name1 = tk.Label(add_frame, text="Enter the name", bg="white", fg="black")
-add_label_name1.grid(row=1, column=1)
+add_label_name.grid(row=1, column=0, padx=3, pady=5)
+add_label_name1 = tk.Label(add_frame, text="Enter the name", bg="#ffcccc", fg="#7a0000")
+add_label_name1.grid(row=1, column=1, pady=5)
 
 add_label_number = tk.Entry(add_frame, text="Enter the number", bg="white", fg="black")
-add_label_number.grid(row=2, column=0)
-add_label_number1 = tk.Label(add_frame, text="Enter the number", bg="white", fg="black")
-add_label_number1.grid(row=2, column=1)
+add_label_number.grid(row=2, column=0, pady=5)
+add_label_number1 = tk.Label(add_frame, text="Enter the number", bg="#ffcccc", fg="#7a0000")
+add_label_number1.grid(row=2, column=1, pady=5)
 
 add_label_type = tk.Entry(add_frame, text="Enter the type", bg="white", fg="black")
-add_label_type.grid(row=3, column=0)
-add_label_type1 = tk.Label(add_frame, text="Enter the type", bg="white", fg="black")
-add_label_type1.grid(row=3, column=1)
+add_label_type.grid(row=3, column=0, pady=5)
+add_label_type1 = tk.Label(add_frame, text="Enter the type", bg="#ffcccc", fg="#7a0000")
+add_label_type1.grid(row=3, column=1, pady=5)
 
 add_label_hp = tk.Entry(add_frame, text="Enter the HP", bg="white", fg="black")
-add_label_hp.grid(row=4, column=0)
-add_label_hp1 = tk.Label(add_frame, text="Enter the HP", bg="white", fg="black")
-add_label_hp1.grid(row=4, column=1)
+add_label_hp.grid(row=4, column=0, pady=5)
+add_label_hp1 = tk.Label(add_frame, text="Enter the HP", bg="#ffcccc", fg="#7a0000")
+add_label_hp1.grid(row=4, column=1, pady=5)
 
 add_label_attack = tk.Entry(add_frame, text="Enter the Attack", bg="white", fg="black")
-add_label_attack.grid(row=5, column=0)
-add_label_attack1 = tk.Label(add_frame, text="Enter the attack", bg="white", fg="black")
-add_label_attack1.grid(row=5, column=1)
+add_label_attack.grid(row=5, column=0, pady=5)
+add_label_attack1 = tk.Label(add_frame, text="Enter the attack", bg="#ffcccc", fg="#7a0000")
+add_label_attack1.grid(row=5, column=1, pady=5)
 
 add_label_defense = tk.Entry(add_frame, text="Enter the Defense", bg="white", fg="black")
-add_label_defense.grid(row=6, column=0)
-add_label_defense1 = tk.Label(add_frame, text="Enter the defense", bg="white", fg="black")
-add_label_defense1.grid(row=6, column=1)
+add_label_defense.grid(row=6, column=0, pady=5)
+add_label_defense1 = tk.Label(add_frame, text="Enter the defense", bg="#ffcccc", fg="#7a0000")
+add_label_defense1.grid(row=6, column=1, pady=5)
 
 add_label_spattack = tk.Entry(add_frame, text="Enter the Sp. Attack", bg="white", fg="black")
-add_label_spattack.grid(row=7, column=0)
-add_label_spattack1 = tk.Label(add_frame, text="Enter the special attack", bg="white", fg="black")
-add_label_spattack1.grid(row=7, column=1)
+add_label_spattack.grid(row=7, column=0, pady=5)
+add_label_spattack1 = tk.Label(add_frame, text="Enter the special attack", bg="#ffcccc", fg="#7a0000")
+add_label_spattack1.grid(row=7, column=1, pady=5)
 
 add_label_spdefense = tk.Entry(add_frame, text="Enter the Sp. Defense", bg="white", fg="black")
-add_label_spdefense.grid(row=8, column=0)
-add_label_spdefense1 = tk.Label(add_frame, text="Enter the sp. defense", bg="white", fg="black")
-add_label_spdefense1.grid(row=8, column=1)
+add_label_spdefense.grid(row=8, column=0, pady=5)
+add_label_spdefense1 = tk.Label(add_frame, text="Enter the sp. defense", bg="#ffcccc", fg="#7a0000")
+add_label_spdefense1.grid(row=8, column=1, pady=5)
 
 add_label_speed = tk.Entry(add_frame, text="Enter the Speed", bg="white", fg="black")
-add_label_speed.grid(row=9, column=0)
-add_label_speed1 = tk.Label(add_frame, text="Enter the speed", bg="white", fg="black")
-add_label_speed1.grid(row=9, column=1)
+add_label_speed.grid(row=9, column=0, pady=5)
+add_label_speed1 = tk.Label(add_frame, text="Enter the speed", bg="#ffcccc", fg="#7a0000")
+add_label_speed1.grid(row=9, column=1, pady=5)
 
 add_label_total = tk.Entry(add_frame, text="Enter the Total", bg="white", fg="black")
-add_label_total.grid(row=10, column=0)
-add_label_total1 = tk.Label(add_frame, text="Enter the total", bg="white", fg="black")
-add_label_total1.grid(row=10, column=1)
+add_label_total.grid(row=10, column=0, pady=5)
+add_label_total1 = tk.Label(add_frame, text="Enter the total", bg="#ffcccc", fg="#7a0000")
+add_label_total1.grid(row=10, column=1, pady=5)
 
 pokedex = [
     {
@@ -178,7 +207,7 @@ pokedex = [
         "Stats": {'HP': 50, 'Attack': 52, 'Defense': 48, 'Sp. Attack': 65, 'Sp. Defense': 50, 'Speed': 55, 'Total': 320},
         "Type": ['Water'],
         "National Number": '0054',
-        "Image": ['Images/psykokwak.png', '#A9A9A9']
+        "Image": ['Images/psykokwak.png', '#E1A31C']
     },
     {
         "Name": "jigglypuff",
@@ -192,14 +221,14 @@ pokedex = [
         "Stats": {'HP': 40, 'Attack': 45, 'Defense': 35, 'Sp. Attack': 40, 'Sp. Defense': 40, 'Speed': 90, 'Total': 290},
         "Type": ['Normal'],
         "National Number": '0052',
-        "Image": ['Images/miaouss.png', '#E6E6E6']
+        "Image": ['Images/miaouss.png', '#FEEDBC']
     },
     {
         "Name": "snorlax",
         "Stats": {'HP': 160, 'Attack': 110, 'Defense': 65, 'Sp. Attack': 65, 'Sp. Defense': 110, 'Speed': 30, 'Total': 540},
         "Type": ['Normal'],
         "National Number": '0143',
-        "Image": ['Images/ronflex.png', '#E6E6E6']
+        "Image": ['Images/ronflex.png', '#00606A']
     }
 ]
 # Initialisation des variables de recherche globales
@@ -312,11 +341,11 @@ def search_by_number():
             return
     messagebox.showinfo("Error", "The pokemon is not in the list! Add the pokemon to the list!")
 
-recherche_bouton1 = tk.Button(search_frame, text = "Search by name", font = ("Helvetica 12"), command=search_by_name)
-recherche_bouton1.grid(row=1, column=1)
+recherche_bouton1 = tk.Button(search_frame, text = "Search by name", font = "OCR 12", bg="#ffcccc", fg="#7a0000", relief="raised", command=search_by_name)
+recherche_bouton1.grid(row=1, column=1, padx=3, pady=5)
 
-recherche_bouton2 = tk.Button(search_frame, text = "Search by number", font=("Helvetica 12"), command=search_by_number)
-recherche_bouton2.grid(row=2, column=1)
+recherche_bouton2 = tk.Button(search_frame, text = "Search by number", font="OCR 12", bg="#ffcccc", fg="#7a0000", relief="raised",command=search_by_number)
+recherche_bouton2.grid(row=2, column=1, padx=5, pady=5)
 
 def add_pokemon():
     global pokedex
@@ -351,8 +380,10 @@ def add_pokemon():
     add_label_speed.delete(0, "end")
     add_label_total.delete(0, "end")
 
-add_bouton = tk.Button(add_frame, text = "Add the pokemon", font = ("Garamond 12"), command=add_pokemon)
+add_bouton = tk.Button(add_frame, text = "Add the pokemon", font = "Garamond 14", command=add_pokemon, relief="raised", bg="#43B14B")
 add_bouton.grid(row=11, column=0, columnspan=2)
 
+# play_music()
+# threading.Thread(target=playsound, args=("Son/pokemontheme.mp3",)).start()
 afficher_pokemon(len(pokedex) - 1)
 fenetre.mainloop()
